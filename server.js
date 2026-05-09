3cae0482e0202c37c59f610231a2a2ad1d9e9dd2
Javinieto <javinieto30@gmail.com>
2026-05-09 12:47:51 +0200

    Update server and index
    Updated server.js to fix errors and adapt to the next part of the project as well as adding an exampe index to check the website works

--- server.js
+++ server.js
@@ -1,29 +1,49 @@
 const net = require('net');
+const fs = require('fs');
 
-// Build the server
+let pokedex = [
+    { id: 1, name: "Bulbasaur", type: "Grass", level: 5 },
+    { id: 2, name: "Charmander", type: "Fire", level: 5 },
+    { id: 3, name: "Squirtle", type: "Water", level: 5 }
+];
+
 const server = net.createServer((socket) => {
-    console.log('¡Cliente conectado!');
-
-    // We receive the client data
     socket.on('data', (data) => {
         const requestText = data.toString();
-        console.log('--- PETICIÓN RECIBIDA ---');
-        console.log(requestText);
+        const lines = requestText.split('\r\n');
+        const [method, path] = lines[0].split(' ');
 
-        // Basic HTTP answer
-        const response = 
-            "HTTP/1.1 200 OK\r\n" +
-            "Content-Type: text/plain\r\n" +
-            "Content-Length: 12\r\n" +
-            "\r\n" +
-            "Hola Mundo!!";
+        console.log(`🎮 Solicitud Pokédex: ${method} ${path}`);
 
-        socket.write(response);
-        socket.end(); // Close connection after theanswer
+        // Main Web
+        if (method === 'GET' && (path === '/' || path === '/index.html')) {
+            fs.readFile('index.html', (err, content) => {
+                if (err) {
+                    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+                } else {
+                    socket.write("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n");
+                    socket.write(content);
+                }
+                socket.end();
+            });
+        } 
+        // Api for all the pokemon
+        else if (method === 'GET' && path === '/pokemon') {
+            const body = JSON.stringify(pokedex);
+            socket.write("HTTP/1.1 200 OK\r\n");
+            socket.write("Content-Type: application/json\r\n");
+            socket.write(`Content-Length: ${Buffer.byteLength(body)}\r\n`);
+            socket.write("\r\n");
+            socket.write(body);
+            socket.end();
+        }
+        else {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+            socket.end();
+        }
     });
 });
 
-// We heard the answer at port 3000
 server.listen(3000, '127.0.0.1', () => {
-    console.log('Servidor escuchando en http://localhost:3000');
+    console.log('🔴 Servidor Pokédex listo en http://localhost:3000');
 });
